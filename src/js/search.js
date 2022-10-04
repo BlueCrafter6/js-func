b4db4dc98bc5756783eef57b3f5617f462f3679e
BlueCrafter6 <yukale.moodliar@gmail.com>
2022-10-04 12:58:26 +0200

    Multiple chnages made

--- src/js/search.js
+++ src/js/search.js
@@ -1,25 +1,24 @@
+
 const searchInp = getID("searchInput");
 const fuwul = document.getElementsByClassName("version-changelog");
 const lowercase = searchInp.value.toLowerCase();
 const fuwli = document.getElementsByClassName("heading");
-
-const sleep = (ms) => {
-  return new Promise((r) => setTimeout(r, ms));
-};
 
 async function search(li, input) {
   const arr = [];
   for (let e of li) {
     arr.push(e.innerHTML);
   }
+
   let filtered = arr.filter((user) => {
     user = user.toLowerCase();
     return user.indexOf(input.value.toLowerCase()) > -1;
   });
-  for (let e of fuwli) {
+  for (let e of li) {
     const div = document.getElementById(e.innerHTML.split(" ").join(""));
-    if (filtered.indexOf(e.innerHTML) == -1) return div.style.display = "none";
-    div.style.display = "block";
+    div.style.animation = "none"
+    if (filtered.indexOf(e.innerHTML) == -1) div.style.display = "none"
+    else div.style.display = "block";
   }
 }
 
