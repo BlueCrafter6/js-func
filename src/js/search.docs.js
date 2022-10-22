dcfca25ec31b11a36dce6f87b057141c92a5bcf7
BlueCrafter6 <yukale.moodliar@gmail.com>
2022-10-22 21:49:46 +0200

    Update files

--- src/js/search.docs.js
+++ src/js/search.docs.js
@@ -1,6 +1,3 @@
-let funcparams = doc.querySelectorAll(".card");
-let fpArray = Array.from(funcparams)
-console.log(fpArray)
 let tableData = getID("tab").getElementsByTagName("tr");
 let arr = [];
 let searchTable = getID("searchTable");
@@ -11,21 +8,13 @@ arr.splice(0, 1);
 console.log(arr);
 
 function search(input, array) {
-     for (data of array) {
-      for (div of fpArray) {
-      if (data.innerHTML.toLowerCase().includes(input.value.toLowerCase())) {
-        data.style.display = "table-row"
-        if (div.innerText.toLowerCase().includes(input.value.toLowerCase())) {
-div.style.display = "block"
-        }
-      } else {
-        if (!div.innerText.toLowerCase().includes(input.value.toLowerCase())) {
-          div.style.display = "none"
-        }
-        data.style.display = "none"
-      }
-     }
-}
+  for (data of array) {
+    if (data.innerText.toLowerCase().includes(input.value.toLowerCase())) {
+      data.style.display = "table-row";
+    } else {
+      data.style.display = "none";
+    }
+  }
 }
 
 searchTable.onkeyup = () => search(searchTable, arr);
