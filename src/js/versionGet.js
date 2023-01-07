df8141086d1cfd5a4b35fc0c4341c7f7542a232f
BlueCrafter6 <yukale.moodliar@gmail.com>
2023-01-07 17:06:20 +0200

    Remove additional loading time

--- src/js/versionGet.js
+++ src/js/versionGet.js
@@ -1,5 +1,6 @@
 let select = getID("ver");
 let versionPlace = getID("versionplace");
+let stl = getID("scripttaglink").innerText.toString()
 
 select.addEventListener("change", function () {
   versionPlace.innerHTML = select.value;
@@ -7,7 +8,6 @@ select.addEventListener("change", function () {
 
 let arr = [];
 let versUl = getID("vers");
-let spinner = getClass("loader")[0];
 
 let options = select.options;
 for (let i of options) {
@@ -18,17 +18,15 @@ arr.forEach(async (option) => {
   li.innerHTML = option.label;
   if (option == arr[0]) {
     versionPlace.innerHTML = option.label;
-    li.innerHTML = `<i class="fa-solid fa-star"></i> ${option.label}`;
+    li.innerHTML = `<i class="fa-solid fa-star"></i> <b>${option.label}</b>`;
   }
-  await sleep(500);
-  spinner.style.display = "none";
   versUl.appendChild(li);
-});
+})
+
 
 let cpc = getID("clipboard")
-let stl = getID("scripttaglink").innerText
 cpc.addEventListener("click", async function () {
-    navigator.clipboard.writeText(stl)
+    navigator.clipboard.writeText(`<script src="https://jsfunc.web.app/package/${versionPlace.innerText}/jsf.js" defer>`)
     cpc.innerHTML = `<i class="fa-solid fa-clipboard-check"></i> Copied`
 await sleep(500)
 cpc.innerHTML = `<i class="fa-solid fa-clipboard"></i> Copy to clipboard`
